5a26e9315de5ba871a8bc4333ab3c96795ea7e70
pxn4 <pxn46@vk.com>
2026-05-09 17:19:36 +0500

    added employee, manager, mark. request. with id, toString, equals, hashc

--- oop_project/src/university_system/Manager.java
+++ oop_project/src/university_system/Manager.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 
+ * Represents a manager in the university system.
  */
 public class Manager extends Employee {
 
@@ -14,25 +14,56 @@ public class Manager extends Employee {
     }
 
     /**
-     * 
+     * Constructor with all fields
+     */
+    public Manager(String firstname, String surname, String password, String username, ManagerType type) {
+        super(firstname, surname, password, username, DepartmentType.MANAGEMENT);
+        this.type = type;
+    }
+
+    /**
+     * type of this manager
      */
     private ManagerType type;
 
-
-
-
     /**
-     * 
+     * returns the type of this manager
      */
-    public void getManagerType() {
-        // TODO implement here
+    public ManagerType getManagerType() {
+        return this.type;
     }
 
     /**
-     * 
+     * sets the type of this manager
      */
-    void setManagerType() {
-        // TODO implement here
+    void setManagerType(ManagerType type) {
+        this.type = type;
     }
 
+    /**
+     * returns string representation of manager
+     */
+    @Override
+    public String toString() {
+        return super.toString() + "Manager type: " + type + "\n";
+    }
+
+    /**
+     * compares managers by id
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Manager other = (Manager) obj;
+        return this.getId() == other.getId();
+    }
+
+    /**
+     * returns hash code based on id
+     */
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(getId());
+    }
 }
--- oop_project/src/university_system/Request.java
+++ oop_project/src/university_system/Request.java
@@ -3,44 +3,129 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 
+ * request in uni system
  */
 public class Request {
 
-    /**
-     * Default constructor
-     */
-    public Request() {
+    /** auto-incremented counter for unique ids */
+    private static int counter = 0;
+
+    /** unique id of this request */
+    private int id;
+
+    {
+        this.id = ++counter;
     }
 
     /**
-     * 
+     * default constructor
      */
-    private int requestId;
+    public Request() {
+        this.isApproved = false;
+        this.createdAt = new Date();
+    }
 
     /**
-     * 
+     * constructor with employee and content
+     */
+    public Request(Employee fromEmployee, String content) {
+        this.fromEmployee = fromEmployee;
+        this.content = content;
+        this.isApproved = false;
+        this.createdAt = new Date();
+    }
+
+    /**
+     * employee who submitted the request
      */
     private Employee fromEmployee;
 
     /**
-     * 
+     * text content of the request
      */
     private String content;
 
     /**
-     * 
+     * employee who signed the request
      */
     private Employee signedBy;
 
     /**
-     * 
+     * whether the request is approved
      */
     private boolean isApproved;
 
     /**
-     * 
+     * date when the request was created
      */
     private Date createdAt;
 
+    /**
+     * returns the id of this request
+     */
+    int getId() {
+        return this.id;
+    }
+
+    /**
+     * returns the employee who submitted the request
+     */
+    Employee getEmployee() {
+        return this.fromEmployee;
+    }
+
+    /**
+     * returns the content of the request
+     */
+    String getContent() {
+        return this.content;
+    }
+
+    /**
+     * returns whether the request is approved
+     */
+    boolean isApproved() {
+        return this.isApproved;
+    }
+
+    /**
+     * sets the approval status of the request
+     */
+    void setApproved(boolean approved) {
+        this.isApproved = approved;
+    }
+
+    /**
+     * sets the employee who signed the request
+     */
+    void setSign(Employee employee) {
+        this.signedBy = employee;
+    }
+
+    /**
+     * returns string representation of request
+     */
+    @Override
+    public String toString() {
+        return "Request id: " + this.id + ", approved: " + isApproved + "\n";
+    }
+
+    /**
+     * compares requests by id
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Request other = (Request) obj;
+        return this.id == other.id;
+    }
+
+    /**
+     * returns hash code based on id
+     */
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(this.id);
+    }
 }
