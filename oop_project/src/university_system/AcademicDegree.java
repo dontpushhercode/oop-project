ba075cfdb358752b15cd2fa57ca5207fb5b5fc5b
evlit <3vlitv1@gmail.com>
2026-05-11 03:44:56 +0500

    javadoc for academicdegree, admin, departmenttype, enrollment, enrollmentstatus, literalgrade added

--- oop_project/src/university_system/AcademicDegree.java
+++ oop_project/src/university_system/AcademicDegree.java
@@ -1,9 +1,21 @@
 package university_system;
 /**
- * 
+ * Represents academic degree level of a student.
  */
 public enum AcademicDegree {
+	
+	/**
+	 * Bachelor's degree.
+	 **/
     BACHELOR,
+    
+    /**
+     * Master's degree.
+     */
     MASTER,
+    
+    /**
+     * Doctor of Philosophy degree.
+     */   
     PHD
 }
--- oop_project/src/university_system/EnrollmentStatus.java
+++ oop_project/src/university_system/EnrollmentStatus.java
@@ -1,9 +1,24 @@
 package university_system;
 /**
- * 
+ * Represents the status of a student's enrollment.
+ *
+ * This status describes the current state of an enrollment
+ * in a course section (active, completed, or withdrawn).
  */
 public enum EnrollmentStatus {
+	
+	/**
+     * The student is currently enrolled in the course.
+     */
     ACTIVE,
+    
+    /**
+     * The student has successfully completed the course.
+     */
     COMPLETED,
+    
+    /**
+     * The student has withdrawn from the course.
+     */
     WITHDRAWN
 }
