2cab0f56ad9e388f65b257ed4e17babb673a969f
shpanova22 <shpanovamadina@gmail.com>
2026-05-18 01:05:47 +0500

    adding ResearchService

--- oop_project/src/university_system/ResearchService.java
+++ oop_project/src/university_system/ResearchService.java
@@ -3,98 +3,267 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 
+ * Service class responsible for managing research-related operations.
+ *
+ * This class works with research profiles, research projects,
+ * project members and research papers.
  */
 public class ResearchService {
-
-    /**
-     * Default constructor
-     */
-    public ResearchService() {
-    }
-
-    /**
-     * 
+	
+	/**
+     * Database used to store and access research data.
      */
     private Database database;
 
 
+    /**
+     * Creates a ResearchService with the given database.
+     *
+     * @param database the database used by this service
+     * @throws IllegalArgumentException if database is null
+     */
+    ResearchService(Database database) {
+    	 if (database == null) {
+             throw new IllegalArgumentException("Database cannot be null");
+         }
+    	this.database = database	
+    }
 
     /**
-     * 
+     * Assigns a research profile to a user.
+     *
+     * A research profile is represented by a Researcher object.
+     *
+     * @param user the user who becomes a researcher
+     * @return the created researcher profile
+     * @throws IllegalArgumentException if user is null
+     * @throws IllegalStateException if the user already has a research profile
      */
-    public void assignResearchProfile() {
-        // TODO implement here
+    Researcher assignResearchProfile(User user) {
+    	if (user == null) {
+             throw new IllegalArgumentException("User cannot be null");
+         }
+    	Researcher existing = getResearchProfile(user);
+
+        if (existing != null) {
+            throw new IllegalStateException("User already has a research profile");
+        }
+
+        Researcher researcher = new Researcher(user);
+
+        database.getResearchers().add(researcher);
+
+        return researcher;
+    }
+
+
+    /**
+     * Removes a research profile from a user.
+     *
+     * @param user the user whose research profile should be removed
+     * @throws IllegalArgumentException if user is null
+     * @throws IllegalStateException if the user does not have a research profile
+     */
+    void removeResearchProfile() {
+    	if (user == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+
+        Researcher researcher = getResearchProfile(user);
+
+        if (researcher == null) {
+            throw new IllegalStateException("User does not have a research profile");
+        }
+
+        database.getResearchers().remove(researcher);
+    }
+
+    /**
+     * Returns the research profile of a user.
+     *
+     * @param user the user whose research profile should be returned
+     * @return the researcher profile, or null if the user has no profile
+     * @throws IllegalArgumentException if user is null
+     */
+    Researcher getResearchProfile() {
+    	 if (user == null) {
+             throw new IllegalArgumentException("User cannot be null");
+         }
+
+         for (Researcher researcher : database.getResearchers()) {
+             if (researcher.getUser().equals(user)) {
+                 return researcher;
+             }
+         }
+
+         return null;
+    }
+
+    /**
+     *Creates a new research project.
+     *
+     * @param projectName the name of the project
+     * @return the created research project
+     * @throws IllegalArgumentException if projectName is empty 
+     */
+    ResearchProject createProject(String projectName) {
+    	if (projectName == null || projectName.isBlank()) {
+            throw new IllegalArgumentException("Project name cannot be empty");
+        }
+
+        ResearchProject project = new ResearchProject(projectName);
+
+        database.getProjects().add(project);
+
+        return project;
+    }
+
+    /**
+     * Adds a researcher as a member of a project.
+     *
+     * @param project the research project
+     * @param researcher the researcher to add
+     * @throws IllegalArgumentException if project or researcher is null
+     * @throws IllegalStateException if researcher is already a member
+     */
+    void addMember(ResearchProject project, Researcher researcher) {
+    	if (project == null) {
+            throw new IllegalArgumentException("Research project cannot be null");
+        }
+
+        if (researcher == null) {
+            throw new IllegalArgumentException("Researcher cannot be null");
+        }
+
+        if (project.getMembers().contains(researcher)) {
+            throw new IllegalStateException("Researcher is already a member of this project");
+        }
+
+        project.addMember(researcher);
+    }
+
+
+    /**
+     * Removes a researcher from a project.
+     *
+     * @param project the research project
+     * @param researcher the researcher to remove
+     * @throws IllegalArgumentException if project or researcher is null
+     * @throws IllegalStateException if researcher is not a member
+     */
+    void removeMember(ResearchProject project, Researcher researcher) {
+    	removeMember(ResearchProject project, Researcher researcher) {
+            if (project == null) {
+                throw new IllegalArgumentException("Research project cannot be null");
+            }
+
+            if (researcher == null) {
+                throw new IllegalArgumentException("Researcher cannot be null");
+            }
+
+            if (!project.getMembers().contains(researcher)) {
+                throw new IllegalStateException("Researcher is not a member of this project");
+            }
+
+            project.deleteMember(researcher);
+    }
+
+    /**
+     *  Adds a research paper to a project and to the database.
+     *
+     * @param project the research project
+     * @param paper the research paper to add
+     * @throws IllegalArgumentException if project or paper is null
+     * @throws IllegalStateException if paper already exists in the project
+     */
+    void addPaper(ResearchProject project, ResearchPaper paper) {
+    	if (project == null) {
+            throw new IllegalArgumentException("Research project cannot be null");
+        }
+
+        if (paper == null) {
+            throw new IllegalArgumentException("Research paper cannot be null");
+        }
+
+        if (project.getPapers().contains(paper)) {
+            throw new IllegalStateException("Paper already exists in this project");
+        }
+
+        project.addPaper(paper);
+
+        if (!database.getPapers().contains(paper)) {
+            database.getPapers().add(paper);
+        }
     }
 
     /**
      * 
      */
-    public void removeResearchProfile() {
-        // TODO implement here
+    List<ResearchPaper> getPapers(ResearchProject project) {
+    	if (project == null) {
+            throw new IllegalArgumentException("Research project cannot be null");
+        }
+
+        return project.getPapers();
     }
 
     /**
-     * 
+     * ssigns a supervisor to a researcher.
+     *
+     * @param researcher the researcher who receives a supervisor
+     * @param supervisor the supervisor
+     * @throws IllegalArgumentException if researcher or supervisor is null
+     * @throws IllegalArgumentException if researcher and supervisor are the same person
      */
-    public void getResearchProfile() {
-        // TODO implement here
+    void assignSupervisor(Researcher researcher, Researcher supervisor) {
+    	if (researcher == null) {
+            throw new IllegalArgumentException("Researcher cannot be null");
+        }
+
+        if (supervisor == null) {
+            throw new IllegalArgumentException("Supervisor cannot be null");
+        }
+
+        if (researcher.equals(supervisor)) {
+            throw new IllegalArgumentException("Researcher cannot be supervisor of himself/herself");
+        }
+
+        researcher.setSupervisor(supervisor);
     }
 
     /**
-     * 
+     * Returns all researchers from the database.
+     *
+     * @return list of all researchers
      */
-    public void createProject() {
-        // TODO implement here
+    List<Researcher> getResearchers() {
+    	return database.getResearchers();
     }
 
     /**
-     * 
+     * Returns researchers filtered by keyword.
+     *
+     * The method searches by user information.
+     *
+     * @param keyword the search keyword
+     * @return list of filtered researchers
+     * @throws IllegalArgumentException if keyword is null
      */
-    public void addMember() {
-        // TODO implement here
-    }
+     List<Researcher> getFilteredResearchers(String keyword) {
+    	 if (keyword == null) {
+             throw new IllegalArgumentException("Keyword cannot be null");
+         }
 
-    /**
-     * 
-     */
-    public void removeMember() {
-        // TODO implement here
-    }
+         List<Researcher> result = new ArrayList<>();
 
-    /**
-     * 
-     */
-    public void addPaper() {
-        // TODO implement here
-    }
+         for (Researcher researcher : database.getResearchers()) {
+             String info = researcher.getUserInfo();
 
-    /**
-     * 
-     */
-    public void getPapers() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void assignSupervisor() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void getResearchers() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void getFilteredResearchers() {
-        // TODO implement here
-    }
-
+             if (info != null && info.toLowerCase().contains(keyword.toLowerCase())) {
+                 result.add(researcher);
+             }
+       }
+         return result;
+     }
 }
+
